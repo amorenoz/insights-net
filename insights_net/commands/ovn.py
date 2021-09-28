9334839e0e2d7b15e040a7a77573af927af22525
Adrian Moreno <amorenoz@redhat.com>
2021-09-28 18:38:13 +0200

    commands: Reimplement using new commands avaialable
    Signed-off-by: Adrian Moreno <amorenoz@redhat.com>

--- insights_net/commands/ovn.py
+++ insights_net/commands/ovn.py
@@ -5,6 +5,41 @@ from rich.pretty import Pretty
 
 from insights_net.main import maincli
 
+# The following click commands only modify the attributes of ctx.
+# The command's logic will then use these commands to obtain the relevant data
+
+
+@maincli.group(name="ovs")
+@click.pass_obj
+def ovs(ctx):
+    """
+    Show the OVS configuration
+    """
+    setattr(ctx, "cmd_table_list", "ovs_table_list")
+    setattr(ctx, "cmd_table", "ovs_table")
+    setattr(ctx, "cmd_find_uuid", "ovs_find_uuid")
+
+
+@ovs.command(name="list")
+@click.argument("table", required=False, nargs=1)
+@click.pass_obj
+def ovs_list(ctx, table):
+    """
+    List the content of a OVS Table
+    """
+    return list_cmd(ctx, table)
+
+
+@ovs.command(name="get")
+@click.argument("table", required=True, nargs=1)
+@click.argument("uuid", required=True, nargs=1)
+@click.pass_obj
+def ovs_get(ctx, table, uuid):
+    """
+    Get an element form the OVS Database
+    """
+    return get_cmd(ctx, table, uuid)
+
 
 @maincli.group(name="ovn")
 @click.pass_obj
@@ -18,14 +53,9 @@ def ovn(ctx):
 @ovn.group(name="nb")
 @click.pass_obj
 def nb(ctx):
-    setattr(ctx, "db", "OVNNBDump")
-
-
-@ovn.group(name="sb")
-@click.pass_obj
-def sb(ctx):
-    setattr(ctx, "db", "OVNSBDump")
-    pass
+    setattr(ctx, "cmd_table_list", "ovnnb_table_list")
+    setattr(ctx, "cmd_table", "ovnnb_table")
+    setattr(ctx, "cmd_find_uuid", "ovnnb_find_uuid")
 
 
 @nb.command(name="list")
@@ -36,75 +66,6 @@ def nb_list(ctx, table):
     List the content of a OVN NB Table
     """
     return list_cmd(ctx, table)
-
-
-@sb.command(name="list")
-@click.argument("table", required=False, nargs=1)
-@click.pass_obj
-def sb_list(ctx, table):
-    """
-    List the content of a OVN SB Table
-    """
-    return list_cmd(ctx, table)
-
-
-def list_cmd(ctx, table):
-    """
-    List the content of a DB Tabel
-    """
-    if not table:
-        tables = ctx.client.evaluate(ctx.db, "table_list()")
-        if not tables:
-            print("OVN data not available")
-            return
-
-        for archive, host_data in tables.items():
-            if not host_data:
-                continue
-
-            print("Archive: " + archive)
-            print("*" * (9 + len(archive)))
-            if isinstance(host_data, str):
-                print(host_data)
-            else:
-                console = Console()
-                console.print("Available Tables:")
-                console.print(host_data)
-        return
-
-    table_data = ctx.client.evaluate(ctx.db, 'table("{}")'.format(table))
-    if not table_data:
-        print("OVN data not available")
-        return
-
-    for archive, host_data in table_data.items():
-        if not host_data:
-            continue
-
-        print("Archive: " + archive)
-        print("*" * (9 + len(archive)))
-        if isinstance(host_data, str):
-            print(host_data)
-        else:
-            print_results(host_data, table)
-
-
-def print_results(table_data, table):
-    console = Console()
-    tt = Table(title=table)
-    for header, value in table_data[next(iter(table_data.keys()))].items():
-        nowrap = False
-
-        if isinstance(value, dict):
-            tt.add_column(header, justify="left", no_wrap=False, ratio=1, min_width=25)
-        else:
-            tt.add_column(header, justify="left", no_wrap=False, min_width=len(header))
-
-    for elem in table_data.values():
-        values = [Pretty(val) for val in elem.values()]
-        tt.add_row(*values)
-
-    console.print(tt)
 
 
 @nb.command(name="get")
@@ -118,6 +79,25 @@ def nb_get(ctx, table, uuid):
     return get_cmd(ctx, table, uuid)
 
 
+@ovn.group(name="sb")
+@click.pass_obj
+def sb(ctx):
+    setattr(ctx, "cmd_table_list", "ovnsb_table_list")
+    setattr(ctx, "cmd_table", "ovnsb_table")
+    setattr(ctx, "cmd_find_uuid", "ovnsb_find_uuid")
+    pass
+
+
+@sb.command(name="list")
+@click.argument("table", required=False, nargs=1)
+@click.pass_obj
+def sb_list(ctx, table):
+    """
+    List the content of a OVN SB Table
+    """
+    return list_cmd(ctx, table)
+
+
 @sb.command(name="get")
 @click.argument("table", required=True, nargs=1)
 @click.argument("uuid", required=True, nargs=1)
@@ -129,14 +109,75 @@ def sb_get(ctx, table, uuid):
     return get_cmd(ctx, table, uuid)
 
 
+# Commands logic
+
+
 def get_cmd(ctx, table, uuid):
-    data = ctx.client.evaluate(
-        "OVNNBDump",
-        'filter("{}", lambda x: x.get("_uuid").startswith("{}"))'.format(table, uuid),
-    )
+    data = ctx.client.run_command(ctx.cmd_find_uuid, table, uuid)
     if not data:
-        print("OVN data not available")
+        print("No data")
+        return
+    for archive, host_data in data.items():
+        if not host_data:
+            print("No data")
+        console = Console()
+        console.print(host_data)
+
+
+def list_cmd(ctx, table):
+    """
+    List the content of a DB Tabel
+    """
+    if not table:
+        tables = ctx.client.run_command(ctx.cmd_table_list)
+        if not tables:
+            print("No data")
+            return
+
+        for archive, host_data in tables.items():
+            print("Archive: " + archive)
+            print("*" * (9 + len(archive)))
+
+            if not host_data:
+                print("No data")
+                continue
+
+            if isinstance(host_data, str):
+                print(host_data)
+            else:
+                console = Console()
+                console.print("Available Tables:")
+                console.print(host_data)
         return
 
+    table_data = ctx.client.run_command(ctx.cmd_table, table)
+    if not table_data:
+        print("No data")
+        return
+
+    for archive, host_data in table_data.items():
+        print("Archive: " + archive)
+        print("*" * (9 + len(archive)))
+        if not host_data:
+            print("No data")
+            continue
+        if isinstance(host_data, str):
+            print(host_data)
+        else:
+            print_results(host_data, table)
+
+
+def print_results(table_data, table):
     console = Console()
-    console.print(data)
+    tt = Table(title=table)
+    for header, value in table_data[next(iter(table_data.keys()))].items():
+        if isinstance(value, dict):
+            tt.add_column(header, justify="left", no_wrap=False, ratio=1, min_width=25)
+        else:
+            tt.add_column(header, justify="left", no_wrap=False, min_width=len(header))
+
+    for elem in table_data.values():
+        values = [Pretty(val) for val in elem.values()]
+        tt.add_row(*values)
+
+    console.print(tt)
